b5b0e668c70168edd5c586a423db45bef6a28d93
Miha Stele <stele1000@gmail.com>
2022-09-03 16:37:20 +0200

    Implement Shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,13 @@ public class Player : MonoBehaviour
     Vector2 minBounds;
     Vector2 maxBounds;
 
+    Shooter shooter;
+
+    void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+    }
+
 
     void Start()
     {
@@ -53,5 +60,14 @@ public class Player : MonoBehaviour
     {
         rawInput = value.Get<Vector2>();
         Debug.Log(rawInput);
+    }
+
+    //InputValue available because we set the Input type to "value"
+    void OnFire(InputValue value)
+    {
+        if (shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
     }
 }
